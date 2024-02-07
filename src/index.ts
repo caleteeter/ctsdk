eaaaa4f7aaa8e7111f1e41d2cdaf7939b41bcf09
Cale Teeter <cale.teeter@microsoft.com>
2024-02-07 09:31:30 -0500

    Update for resources

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import extract from "extract-zip";
+import { join } from "path";
+
 export function hello(): string {
     return "Hello, world!";
 };
+
+export async function scaffold(projectLocation: string): Promise<void> {
+    await extract(join(__dirname, "resources", "testcontent.zip"), { dir: projectLocation });
+} 
